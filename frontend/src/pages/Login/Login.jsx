ae9456fc343855571600c7d8e1a80be82fe247ea
Saurav Kumar <skyjnv124@gmail.com>
2025-11-15 00:18:30 -0500

    added login page- html and css, also created public folder to add all images there

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../../api";
+import "./Login.css";
+import loginPic from '/images/loginpage.png';
 
 const Login = () => {
   const [message, setMessage] = useState("Loading...");
@@ -15,9 +17,18 @@ const Login = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Login</h1>
-      <p>{message}</p>
+    <div className="login-container">
+      <div className="login-form">
+        <h1>Login</h1>
+        <p>How i get started with Engsoc</p>
+        <input type="username" placeholder="Username or Email" />
+        <input type="password" placeholder="Enter password" />
+        <button className="loginbutton">Login</button>
+        <a href="" >Register here</a>
+      </div>
+      <div className="login-image">
+        <img src={loginPic} alt="girl in jacket"   />
+      </div>
     </div>
   );
 };
